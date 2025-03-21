2e32c935809ea3b00f50bf80b19a58e56e4ba8b0
Phlopp <phlopp@mail.ru>
2025-03-21 08:21:38 +0300

    Merge remote-tracking branch 'origin/main'

--- armagedec/src/page/Asteroids.jsx
+++ armagedec/src/page/Asteroids.jsx
@@ -1,8 +1,10 @@
 
 
 import {Header} from "../components/header/Header";
+import {useState} from "react";
 
 export const Asteroids =()=>{
+    console.log("Parent Component rendered")
 return <div>
      <Header/>
     <InternalComponent/>
@@ -14,7 +16,10 @@ return <div>
 }
 const InternalComponent = ()=>{
     console.log("Internal Component rendered")
+    const [counter, setCounter] = useState(0);
     return<div style={{backgroundColor: "red", hight: "200px", width: "200px"}}>
         Internal component
+        {counter}
+        <button onClick={()=> setCounter(counter + 1)}>Counter</button>
     </div>
 }
