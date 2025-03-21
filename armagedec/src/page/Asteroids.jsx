b17998a7763b35b0134355ee411edf044cf6cb2e
Phlopp <phlopp@mail.ru>
2025-03-21 03:43:37 +0300

    Merge remote-tracking branch 'origin/main'

--- armagedec/src/page/Asteroids.jsx
+++ armagedec/src/page/Asteroids.jsx
@@ -1,10 +1,20 @@
-//import {Header} from ""
 
+
+import {Header} from "../components/header/Header";
 
 export const Asteroids =()=>{
 return <div>
- <div>
-     This is page Asteroids
- </div>
+     <Header/>
+    <InternalComponent/>
+
+
+
 </div>
+
+}
+const InternalComponent = ()=>{
+    console.log("Internal Component rendered")
+    return<div style={{backgroundColor: "red", hight: "200px", width: "200px"}}>
+        Internal component
+    </div>
 }
