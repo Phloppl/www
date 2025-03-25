b943ca330798941464f7734aa0528fb0ec6e405c
Phlopp <phlopp@mail.ru>
2025-03-25 21:58:09 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ armagedec/src/components/AsteroidCard/AsteroidCard.tsx
@@ -0,0 +1,27 @@
+import { AsteroidCardAction } from "./AsteroidCardAction/AsteroidCardAction";
+import { AsteroidCardContent } from "./AsteroidCardContent/AsteroidCardContent";
+import { AsteroidCardImage } from "./AsteroidCardImage/AsteroidCardImage";
+import { AsteroidCardImageDino } from "./AsteroidCardImage/AsteroidCardImageDino";
+import styles from "./Card.module.css";
+
+type AsteroidCardProps = {
+    name: string;
+    date: string;
+    distanceK: number;
+    distanceL: number;
+    size: number;
+    isDangerous: boolean;
+    distanceMode: boolean;
+}
+
+export const AsteroidCard = (props: AsteroidCardProps)=>{
+    const{name, date, distanceK, distanceL, size, isDangerous, distanceMode} = props;
+
+    return (<div className={styles.card}>
+        <div className={isDangerous ? styles.cardRed : styles.cardConst}></div>
+        <AsteroidCardImage/>
+        <AsteroidCardImageDino/>
+        <AsteroidCardContent name={name} date={date} distanceK={distanceK} distanceL={distanceL} size={size} distanceMode={distanceMode}/>
+        <AsteroidCardAction isDangerous={isDangerous}/>
+    </div>)
+}
